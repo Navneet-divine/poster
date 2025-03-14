c0ab5771432d25aa2f77d4218f3521d8c224eb7f
Navneet-divine <navneetkush0102@gmail.com>
2025-03-14 18:00:14 +0530

    change code of post-details

--- app/api/posts/post-details/route.ts
+++ app/api/posts/post-details/route.ts
@@ -2,34 +2,33 @@ import { NextResponse } from "next/server";
 import { connectDB } from "@/lib/db";
 import Post from "@/models/postModel";
 
-
 connectDB();
 
-export async function GET({ params }: { params: Promise<{ postId: string | string[] }> }) {
+export async function GET({ params }: { params: { postId: string | string[] } }) {
     try {
-        const { postId } = await params;
+        const { postId } = params;
 
-
+        // If postId is an array, return a bad request response
         if (Array.isArray(postId)) {
             return NextResponse.json({ msg: "Invalid postId." }, { status: 400 });
         }
 
+        // Fetch the post from the database using the postId
         const post = await Post.findById(postId);
-        console.log(post);
 
+        // If post not found, return an error message
         if (!post) {
-            return NextResponse.json({ msg: "post is not found." }, { status: 400 });
+            return NextResponse.json({ msg: "Post not found." }, { status: 404 });
         }
 
+        // Return the post data as JSON
         return NextResponse.json(post, { status: 200 });
 
     } catch (error: unknown) {
-
+        // Return the error message if an unexpected error occurs
         if (error instanceof Error) {
             return NextResponse.json({ error: error.message }, { status: 500 });
         }
-
         return NextResponse.json({ error: "An unexpected error occurred" }, { status: 500 });
     }
 }
-

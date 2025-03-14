adc060acf9110cdfd5c65a44668795adb86d5519
Navneet-divine <navneetkush0102@gmail.com>
2025-03-14 18:16:21 +0530

    again change the code of post-details route.ts

--- app/api/posts/post-details/route.ts
+++ app/api/posts/post-details/route.ts
@@ -5,11 +5,10 @@ import Post from "@/models/postModel";
 connectDB();
 
 export async function GET(
-    request: Request,
-    { params }: { params: { postId: string } }
+    { params }: { params: Promise<{ postId: string }> }
 ) {
     try {
-        const { postId } = params;
+        const { postId } = await params;
 
         if (!postId) {
             return NextResponse.json({ msg: "Invalid postId." }, { status: 400 });

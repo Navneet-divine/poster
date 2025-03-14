9be8baec12a56e00f1fdff1ccb2f5ad5753ba3b3
Navneet-divine <navneetkush0102@gmail.com>
2025-03-14 18:10:24 +0530

    again change code of post-details for deploy

--- app/api/posts/post-details/route.ts
+++ app/api/posts/post-details/route.ts
@@ -4,30 +4,31 @@ import Post from "@/models/postModel";
 
 connectDB();
 
-export async function GET({ params }: { params: Promise<{ postId: string }> }) {
+export async function GET(
+    request: Request,
+    { params }: { params: { postId: string } }
+) {
     try {
-        const { postId } = await params;
+        const { postId } = params;
 
-        if (Array.isArray(postId)) {
+        if (!postId) {
             return NextResponse.json({ msg: "Invalid postId." }, { status: 400 });
         }
 
-
         const post = await Post.findById(postId);
-
 
         if (!post) {
             return NextResponse.json({ msg: "Post not found." }, { status: 404 });
         }
 
-
         return NextResponse.json(post, { status: 200 });
-
     } catch (error: unknown) {
-
         if (error instanceof Error) {
             return NextResponse.json({ error: error.message }, { status: 500 });
         }
-        return NextResponse.json({ error: "An unexpected error occurred" }, { status: 500 });
+        return NextResponse.json(
+            { error: "An unexpected error occurred" },
+            { status: 500 }
+        );
     }
 }
